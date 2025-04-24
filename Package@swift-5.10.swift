081c37f72e1e869ce975045e1fdd5ab4cbc803a5
Giovanni Petrantoni <7008900+sinkingsugar@users.noreply.github.com>
2025-04-24 14:04:19 +0800

    Add shards.min.js to package resources
    - Included `shards.min.js` in the package resources for improved syntax highlighting support.

--- Package@swift-5.10.swift
+++ Package@swift-5.10.swift
@@ -27,6 +27,7 @@ let package = Package(
             ],
             resources: [
                 .copy("Assets/Highlighter/highlight.min.js"),
+                .copy("Assets/Highlighter/shards.min.js"),
                 .copy("Assets/styles/a11y-dark.min.css"),
                 .copy("Assets/styles/a11y-light.min.css"),
                 .copy("Assets/styles/agate.min.css"),
